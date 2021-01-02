77e0ae16eae706be898baa2de8918d5543fc3331
amussi <amussi721@gmail.com>
2021-01-03 00:03:01 +0900

    2021.01.03 헤더 및 슬라이드 배너 작업

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    mode: 'horizontal',// 가로 방향 수평 슬라이드
+    speed: 500,        // 이동 속도를 설정
+    pause: 3000,
+    auto: true,        // 자동 실행 여부
+  });
+});
